a357909c7c4b7a66b9bbd99d84724bcb184f5e10
rezaee <hamidrezaee140@gmail.com>
2024-06-22 17:59:12 +0330

    add Review Dashboard

--- app/src/main/java/ir/hamid/ilearn/MainActivity.kt
+++ app/src/main/java/ir/hamid/ilearn/MainActivity.kt
@@ -9,16 +9,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -37,12 +43,28 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun LessonsList() {
 
-    LazyVerticalGrid(
+    LazyVerticalStaggeredGrid(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 40.dp),
-        columns = GridCells.Adaptive(minSize = 128.dp),
+            .fillMaxSize(),
+        columns = StaggeredGridCells.Adaptive(minSize = 128.dp),
     ) {
+        item( span = StaggeredGridItemSpan.FullLine) {
+            Card(  // dashboard for Review words
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = CardColors(
+                    containerColor = colorResource(id = R.color.purple_200),
+                    contentColor = colorResource(id = R.color.black),
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.DarkGray
+                )
+            ) {
+
+            }
+        }
         items(count = 46) { index ->
             LessonsCard(text = index.toString())
         }
@@ -54,14 +76,14 @@ private fun LessonsCard(text: String) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .wrapContentSize()            ,
+            .wrapContentSize(),
         shape = RoundedCornerShape(5.dp)
     ) {
         Column {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = "description",
-                modifier = Modifier.size(120.dp)
+                modifier = Modifier.size(112.dp)
             )
             Row(
                 modifier = Modifier.fillMaxSize(),
