f67732ae9349401467ab13aba42035e26b820762
rezaee <hamidrezaee140@gmail.com>
2024-08-26 14:09:53 +0330

    change UI

--- app/src/main/java/ir/hamid/ilearn/MainActivity.kt
+++ app/src/main/java/ir/hamid/ilearn/MainActivity.kt
@@ -1,18 +1,26 @@
 package ir.hamid.ilearn
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -21,23 +29,41 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updateLayoutParams
+import kotlin.contracts.contract
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -52,98 +78,145 @@ class MainActivity : ComponentActivity() {
 private fun AppBar() {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            TopAppBar(
                 title = {
                     Text(
                         text = "iLearn",
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
-                        fontSize = 30.sp
+                        fontSize = 20.sp
                     )
                 },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+//                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorResource(id = R.color.background))
             )
         },
         content = { innerPadding ->
-            LessonsList(innerPadding)
+            AppUI(innerPadding)
         }
     )
 }
 
 @Composable
-private fun LessonsList(innerPadding: PaddingValues) {
-
-    LazyVerticalGrid(
+private fun AppUI(innerPadding: PaddingValues) {
+    Column(
         modifier = Modifier
+            .padding(innerPadding)
+            .padding(top = 20.dp)
             .fillMaxSize(),
-        contentPadding = innerPadding,
-        columns = GridCells.Adaptive(minSize = 128.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        item(span = { GridItemSpan(3) }) {
-            Card(  // dashboard for Review words
+
+        Text(
+            text = "Let's start",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Towards success",
+            fontSize = 16.sp
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.8f)
+                .padding(top = 20.dp)
+                .padding(start = 25.dp, end = 25.dp, top = 25.dp, bottom = 5.dp),
+            shape = RoundedCornerShape(50f),
+            colors = CardColors(
+                containerColor = colorResource(id = R.color.cardColor),
+                contentColor = colorResource(id = R.color.cardColor),
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.DarkGray,
+            )
+        ) {
+            Box(
                 modifier = Modifier
-                    .height(130.dp)
-                    .fillMaxWidth()
-                    .padding(start = 24.dp, top = 24.dp, end = 24.dp, bottom = 24.dp),
-                shape = RoundedCornerShape(5.dp),
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Search",
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily(Font(R.font.kanitmedium)),
+                    color = Color.Black
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .padding(start = 25.dp, end = 5.dp, top = 5.dp, bottom = 25.dp),
+                shape = RoundedCornerShape(50f),
                 colors = CardColors(
-                    containerColor = colorResource(id = R.color.purple_200),
-                    contentColor = colorResource(id = R.color.black),
+                    containerColor = colorResource(id = R.color.cardColor),
+                    contentColor = colorResource(id = R.color.cardColor),
                     disabledContainerColor = Color.Gray,
                     disabledContentColor = Color.DarkGray
                 )
             ) {
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalAlignment = Alignment.CenterVertically
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Learning",
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.kanitmedium)),
+                        color = Color.Black
+                    )
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .padding(start = 5.dp, end = 25.dp, top = 5.dp, bottom = 25.dp),
+                shape = RoundedCornerShape(50f),
+                colors = CardColors(
+                    containerColor = colorResource(id = R.color.cardColor),
+                    contentColor = colorResource(id = R.color.cardColor),
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.DarkGray
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
                 ) {
                     Text(
                         text = "Review",
-                        modifier = Modifier.padding(start = 16.dp),
-                        fontSize = 24.sp,
-                        color = colorResource(R.color.white),
-                        fontWeight = FontWeight.Bold
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(Font(R.font.kanitmedium)),
+                        color = Color.Black
                     )
-                    Image(
-                        painter = painterResource(id = R.drawable.arrow),
-                        alignment = Alignment.CenterEnd,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(end = 16.dp),
-                        contentDescription = ""
-                    )
-
                 }
-
             }
         }
-        items(count = 46) { index ->
-            LessonsCard(text = index.toString())
-        }
-    }
-}
 
-@Composable
-private fun LessonsCard(text: String) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .wrapContentSize(),
-        shape = RoundedCornerShape(5.dp)
-    ) {
-        Column {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "description",
-                modifier = Modifier
-                    .fillMaxSize()
-            )
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(text = text)
-            }
-        }
+        CircularProgressIndicator(
+            progress = {
+                0.7f
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(3f)
+                .padding(85.dp),
+            color = colorResource(id = R.color.black),
+            strokeWidth = 13.dp,
+            trackColor = colorResource(id = R.color.cardColor),
+        )
+
     }
 }
